610f487a449d888970459f14aaf43336250955dc
Wutieyin <136192696+Wutieyin@users.noreply.github.com>
2024-10-22 19:08:44 +0100

    delete

--- week04/scripts/lazyload.js
+++
@@ -1,6 +0,0 @@
-let text = document.lastModified;
-document.getElementById("demo").innerHTML = text;
-
-var date = new Date(); 
-var year = date.getFullYear();
-document.getElementById('curYr').innerHTML = year
